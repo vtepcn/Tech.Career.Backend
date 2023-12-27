40a13c223c3da8be0428aa0e364de331e106c242
vtepcn <mehmetcepni3434@gmail.com>
2023-12-27 19:35:07 +0300

    Add project files.

---
+++ p1/Program.cs
@@ -0,0 +1,180 @@
+﻿//Console.WriteLine("Hello, World!");
+
+//// string
+//string ad = "mehmet";
+
+//string soyad = "çepni";
+
+//string tamAd = ad + " " + soyad;
+//Console.WriteLine(tamAd);
+
+//// int
+//int sayi1 = 10;
+//Console.WriteLine(sayi1);
+
+//// double  (float'a göre daha kritik heaplamalar yapar.)
+//double ondalik = 10.3;
+//Console.WriteLine(ondalik);
+
+//// float
+//float ondalik1 = 15.2f;
+//Console.WriteLine(ondalik1);
+
+//// char
+//char a = 'a';
+//Console.WriteLine(a);
+//Console.WriteLine((int)(a));
+
+// Konsoldan girdi alma
+// Kullanıcıdan isim ve soyisim al ve ekrana yazdır.               ***
+
+//string isim, soyisim;
+//Console.Write("isminizi yazınız: ");
+//isim=Console.ReadLine();
+//Console.Write("soyisiminizi yazınız: ");
+//soyisim=Console.ReadLine();
+
+////1.Yöntem
+//Console.WriteLine(isim+ " " +soyisim);
+////2.Yöntem
+//Console.WriteLine($" isim : {isim} soyisim : {soyisim}");
+//Console.WriteLine($"{isim} {soyisim}");
+
+//Console.WriteLine("Yaşınızı girininiz: ");
+
+//var yas = Convert.ToInt32(Console.ReadLine());  //var tüm veri tiplerinin yerini alır.
+//Console.WriteLine(yas);
+//Console.WriteLine(yas.GetType());  //Veri Tipini Gösterir
+
+// Aritmetik Operatörler
+//Console.Write( "1.Ondalıklı sayıyı giriniz: ");
+//double ondalik1 = Convert.ToDouble(Console.ReadLine());
+//Console.Write("2.Ondalıklı sayıyı giriniz: ");
+//double ondalik2 = Convert.ToDouble(Console.ReadLine());
+
+//Console.Write("1.Tam sayıyı giriniz: ");
+//int tam1 = Convert.ToInt32(Console.ReadLine());
+//Console.Write("2.Tam sayıyı giriniz: ");
+//int tam2 = Convert.ToInt32(Console.ReadLine());
+
+
+//Console.WriteLine("1.Sayı: " +tam1);
+//Console.WriteLine("2.Sayı: " + tam2);
+
+//int tamToplam =tam1+ tam2;
+//int tamFark = tam1 - tam2;
+//int tamCarpim = tam1 * tam2;
+//int tamBolme = tam1 / tam2;
+//Console.WriteLine("sayıların toplamı"+"="+(tam1+tam2));
+//Console.WriteLine("sayıların farkı"+"=" + (tam1-tam2));
+//Console.WriteLine("sayıların çarpımı" + "=" + (tam1 * tam2));
+//Console.WriteLine("sayıların bölümü" + "=" + (tam1 / tam2));
+
+
+// Karşılaştırma Operatörleri
+// ==  ,  !=  , <  ,  >  ,  >= ,  <=   
+bool dogruluk1 = 4==5;  //eşit midir
+bool dogruluk2 = 4 != 5;  //eşit değil midir
+Console.WriteLine(dogruluk1);
+Console.WriteLine(dogruluk2);
+
+
+// Mantıksal Opertörler
+
+// or (veya)  ||
+//and (ve)  &&
+
+//bool islem1 = (3 < 5) && (6 < 5);
+//bool islem2 = (3 < 5) || (6 < 5);
+//Console.WriteLine(islem1);
+//Console.WriteLine(islem2);
+
+////yaşı 18 den büyük ve parası 50.000 tl den büyük olanlar araba alabilir      ***
+
+//int age = 18;
+//int money = 60000;
+
+//if(age>18 && money > 50000)
+//    Console.WriteLine("Araba alabilirsiniz . ");
+//else
+//    Console.WriteLine("Araba alamazsınız . ");
+
+
+//Kullanıcıdan ders notıu aldığğımda harf notunu ekrana bastırsın.      ***
+// not 50'den küçükse F
+// not 50 ile 65 arası E
+//not 65 ile 70 arası D
+// not 70 ile 80 arası C
+// not 80 ile 90 araası B
+// not 90 ile 100 arası ise A
+
+//Console.WriteLine("Lütfen notunuzu giriniz: ");
+//int not=Convert.ToInt32(Console.ReadLine());
+//if (not>0 && not <= 50)
+//{
+//    Console.WriteLine("F");
+//}
+//else if(not>50 && not <= 65)
+//{
+//    Console.WriteLine("E");
+//}
+//else if(not>65 && not <= 70)
+//{
+//    Console.WriteLine("D");
+//}
+//else if(not>70 && not <= 80)
+//{
+//    Console.WriteLine("C");
+//}
+//else if(not>80 && not <= 90)
+//{
+//    Console.WriteLine("B");
+//}
+//else if(not>90 && not<=100)
+//{
+//    Console.WriteLine("A");
+//}
+//else
+//{
+//    Console.WriteLine( "Yanlış değer girdiniz!");
+//}
+
+
+// FONKSİYONLAR (METODLAR)
+
+//  Merhaba yazdıran metodu yazınız
+
+//MerhabaDe();
+//Console.Write("Lütfen isminizi girin : ");
+//string isim = Console.ReadLine();
+
+//Console.Write("Lütfen soyisminizi girin : ");
+//string soyisim = Console.ReadLine();
+
+//Console.Write("Lütfen yaşınızı girin : ");
+//int yas = Convert.ToInt32(Console.ReadLine());
+
+//IsimYaz(isim:isim, soyisim:soyisim, yas:yas);
+
+
+//void MerhabaDe()
+//{
+//    Console.WriteLine("Merhaba");
+//}
+
+////  Parametre alan ama değer döndürmeyen metodlar
+
+//void IsimYaz(string isim,string soyisim,int yas)
+//{
+//    Console.WriteLine("İsminiz :" + isim + "Soyadınız :" + soyisim+ "Yaşınız :" + yas);
+//}
+
+
+//// Toplama ve çıkarma işlemlerinin fonksiyonlarını yazınız
+
+//int Topla(int x,int y)
+//{
+//    return x + y;
+//}
+//int toplam=Topla(3, 4);
+//Console.WriteLine(toplam);
---
+++ p3/Program.cs
@@ -0,0 +1,112 @@
+﻿// COLLECTİONS
+// ARRAY
+
+// 1'den 9'a kadar sayıların tutulduğu array'i yazınız    ***
+
+//int[] sayilar = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//Console.WriteLine(sayilar[3]);
+
+
+// Döngüler
+// FOR DÖNGÜSÜ
+
+//for(int i = 0; i < sayilar.Length; i++)
+//{
+//    Console.Write(sayilar[i]);
+//}
+
+// FOREACH
+
+//foreach(int sayi in sayilar)
+//{
+//    Console.Write(sayi);
+//} 
+
+//string[] enGuzelArabalar = { "Honda", "Merso", "BMW" };
+//string[] enKotuArabalar = { "Fiat", "golf", "mocca" };
+
+//Console.WriteLine("EN Güzel Arabalar Listesi");
+//foreach (string arac in enGuzelArabalar)
+//{
+//    Console.WriteLine(arac);
+//}
+
+
+// LİST   (3 Kullanım şekli)
+
+//List<string> sehirler1 = new();
+//var sehirler2=new List<string>();
+//List<string> sehirler = new List<string>()
+//{
+//    "Malatya","Istanbul","Trabzon","Suri"
+//};
+
+//sehirler.Add("Rize");           //Ekleme
+//sehirler.Remove("Istanbul");    //Silme
+
+//Console.WriteLine("Şehirler listesi kaç elemanlı :"+ sehirler.Count);
+
+//foreach (string s in sehirler)
+//{
+//    Console.WriteLine(s);
+//}
+
+// Kullanıcı istediği kadar şehir girsin ve o şehirleri en son ekrana bastırınız.          *****
+
+//List<string> city = new List<string>();
+//Console.WriteLine("Kaç şehir girmek istersiniz?");
+//int sehirsayisi=Convert.ToInt32(Console.ReadLine());
+//for (int i = 0; i < sehirsayisi; i++)
+//{
+//    Console.Write((i+1)+".Şehri giriniz: ");
+//    city.Add(Console.ReadLine());
+//}
+
+//foreach(string s in city)
+//{
+//    Console.WriteLine(s);         // 1.Yöntem
+//}
+
+//city.ForEach(s => Console.WriteLine(s));      // 2.Yöntem
+
+
+//   1'den 10'a kadar sayı listesi olsun.Bu sayı listesinden 2 tane daha liste çıkararak          ********
+//  tek sayılar ve çift sayılar listesini oluşturup ekrana bastırınız.
+
+//List<int> sayilar = new List<int>();
+//List<int> tekSayilar= new List<int>();
+//List<int> CiftSayilar = new List<int>();
+
+//for (int i = 0; i < 10; i++)
+//{
+//    sayilar.Add(i);
+//}
+
+//foreach (int sayi in sayilar)
+//{
+//    if (sayi % 2 == 0)
+//    {
+//        CiftSayilar.Add(sayi);
+//    }
+//    else
+//    {
+//        tekSayilar.Add(sayi);
+//    }
+//}
+
+//Console.WriteLine("TEK SAYILAR");
+//foreach (int i in tekSayilar)
+//{
+//    Console.WriteLine(i);
+//}
+
+//Console.WriteLine("ÇİFT SAYILAR 1.YÖNTEM");
+//foreach(int j in CiftSayilar)
+//{
+//    Console.WriteLine(j);
+//}
+
+//Console.WriteLine("ÇİFT SAYILAR 2.YÖNTEM");
+//sayilar.FindAll(x => x % 2 == 0).ForEach(y=> Console.WriteLine(y));  
+
+
